207541a9d426178e9442da248855a888fb48f06a
Mina Kang <kasong13@unist.ac.kr>
2021-01-02 05:25:43 +0900

    2021_01_02_gif
    unist gif 추가한거

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -12,17 +12,23 @@ class MainPage extends StatelessWidget {
   // 1-2. 탭 화면 (build() 함수를 _MainState로 옮김)
   @override
   Widget build(BuildContext context) {
+    const color = const Color(0xff001E54);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: color,
         // 1-1. 상단화면 (제목 수정)
-        title: Text('Flutter'),
+        title: Image(image: AssetImage('assets/UNIST.gif')),
         // 1-1. 상단화면 (좌측 버튼 추가)
 
         // 1-1. 상단화면 (우측 팝업 버튼 및 이벤트 추가)
-
       ),
       // 1-2. 탭 화면 (List, Grid Widget 연동)
-      body: Center(child: Text("Hello Flutter")),
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(" 졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려졸려",
+              style: TextStyle(
+                fontSize: 10,
+              ))),
       // 1-2. 탭 화면 (bottomNavigationBar 추가)
     );
   }
