e755c004b0bc273685b9ab151e66ef2f178edc19
Mina Kang <kasong13@unist.ac.kr>
2021-01-04 02:49:36 +0900

    2021-01-04
    -popupMenueItem

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -26,8 +26,29 @@ class MainPage extends StatelessWidget {
           ),
         ]),
         // 1-1. 상단화면 (좌측 버튼 추가)
+        //leading: Icon(Icons.sort),
 
         // 1-1. 상단화면 (우측 팝업 버튼 및 이벤트 추가)
+        actions: <Widget>[
+          PopupMenuButton<int>(
+            icon: Icon(Icons.sort),
+            onSelected: (value) {
+              if (value == 0)
+                print("예매율순");
+              else if (value == 1)
+                print("큐레이션");
+              else
+                print("최신순");
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(value: 0, child: Text("예매율순")),
+                PopupMenuItem(value: 1, child: Text("큐레이션")),
+                PopupMenuItem(value: 2, child: Text("최신순")),
+              ];
+            },
+          )
+        ],
       ),
       // 1-2. 탭 화면 (List, Grid Widget 연동)
       body: Padding(
