c739bbd35f182258b60defe850d01a8af02d0987
Mina Kang <kasong13@unist.ac.kr>
2021-01-06 13:11:41 +0900

    2021-01-06
    -State
    -list_page.dart

--- lib/list_page.dart
+++ lib/list_page.dart
@@ -7,10 +7,57 @@ class ListPage extends StatelessWidget {
     // 1-3. 리스트 화면 (동적 데이터 추가)
 
     // 1-3. 리스트 화면 (초기 리스트 구현)
-    return Center(child: Text("list"));
-  }
-
-// 1-3. 리스트 화면 (고정 더미 데이터)
+    return ListView(children: <Widget>[
+      Container(
+        padding: EdgeInsets.all(12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Image.network(
+                'https://padakpadak.s3.ap-northeast-2.amazonaws.com/image/1.jpg',
+                height: 120),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        '신과함께-죄와 벌',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Text("12")
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: <Widget>[
+                      Text('평점 : 139'),
+                      SizedBox(width: 10),
+                      Text('예매순위 : 1'),
+                      SizedBox(width: 10),
+                      Text('예매율 : 35.5')
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text('개봉일: 2017-12-20')
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      Container(),
+      Container(),
+      Container()
+    ]);
+  } // 1-3. 리스트 화면 (고정 더미 데이터)
 
 // 1-3. 리스트 화면 (동적 데이터 호출1)
 
